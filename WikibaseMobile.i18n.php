d8eec0bd8cab1ce2ac9dcd4ce91abb4c9b7bc138
pragunbhutani <pragun06@gmail.com>
2013-08-22 03:58:37 +0530

    Writing up basic skin definition

---
+++ WikibaseMobile.i18n.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Internationalization file for skin MobileWikibase.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+$messages = array();
+
+/**
+ * English
+ * @author Pragun Bhutani
+ */
+$messages['en'] = array(
+   'wikibase-mobile-desc' => 'A Mobile Skin for Wikibase.',
+);
+
+/**
+ * Message documentation
+ * @author Pragun Bhutani
+ */
+$messages['qqq'] = array(                                                     //TODO: Discuss Skin name.
+   'erudite-desc' => '{{desc|name=WikibaseMobile|url=http://www.mediawiki.org/wiki/Skin:WikibaseMobile}}',
+);
---
+++ WikibaseMobile.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * WikibaseMobile Skin
+ *
+ * @file
+ * @ingroup Skins
+ * @author Pragun Bhutani
+ * @author Katie Filbert
+ * @author Jon Robson
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
+
+$wgExtensionCredits['skin'][] = array(
+   'path' => __FILE__,
+   'name' => 'WikibaseMobile',												//TODO: Discuss Skin name. Check convention.
+   'url' => 'https://www.mediawiki.org/wiki/Skin:WikibaseMobile',			//Note: URL may change
+   'author' => array( 'Pragun Bhutani', 'Katie Flibert', 'Jon Robson' ),
+   'version' => '1.0.0',													//TODO: Check version numbering convention
+   'descriptionmsg' => 'wikibase-mobile-desc',								//TODO: Remember to add a description message
+);
+
+$wgValidSkinNames['wikibasemobile'] = 'WikibaseMobile';
+$wgAutoloadClasses['WikibaseMobile'] = dirname(__FILE__) . '/WikibaseMobile.skin.php';
+$wgExtensionMessagesFiles['WikibaseMobile'] = dirname(__FILE__) . '/WikibaseMobile.i18n.php';
+
+$wgResourceModules['skins.WikibaseMobile'] = array(
+   'styles' => array(
+       'erudite/assets/WikibaseMobile.css' => array( 'media' => 'screen' ),
+   ),
+   'remoteBasePath' => &$GLOBALS['wgStylePath'],
+   'localBasePath' => &$GLOBALS['wgStyleDirectory'],
+);
